ea290bf38251606d052c554be06340d82e48c70f
Isaac Obuya <Szack4477@gmail.com>
2020-06-25 13:04:10 +0300

    use async-await with mongoose to find and delete task the return number of incompleted tasks

--- src/playground/promise-chaining2.js
+++ src/playground/promise-chaining2.js
@@ -3,9 +3,21 @@ const Task = require("../models/task");
 
 // find a given task and delete
 // get and print the total number of incomplete tasks
-Task.findByIdAndDelete({_id: "5ef1d49db55713237c901bd1"})
-.then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false});
-}).then(result => console.log(result))
-.catch(e => console.log(e));
+// Task.findByIdAndDelete({_id: "5ef1d49db55713237c901bd1"})
+// .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false});
+// }).then(result => console.log(result))
+// .catch(e => console.log(e));
+
+const deleteTaskAndCount = async (taskId) => {
+    // find and delete the task
+    const task = await Task.findByIdAndDelete(taskId);
+    // return number of documents with incomplete tasks
+    const incompleteTasksCount = await Task.countDocuments({ completed: false});
+    return incompleteTasksCount;
+}
+
+deleteTaskAndCount("5ef32ef912c33131b36ad5cb")
+.then(count => console.log(count))
+.catch(error => console.log(error));
