86bcd4dc055fe77b50d2fe218d76d0c8a5050daa
Isaac Obuya <Szack4477@gmail.com>
2020-06-25 11:57:52 +0300

    practice promise chaining with mongoose

---
+++ src/playground/promise-chaining.js
@@ -0,0 +1,11 @@
+require('../db/mongoose');
+const User = require('../models/user');
+
+// practice promise chaining with mongoose
+User.findByIdAndUpdate("5ef328f1c004a0286300f8aa", { age: 26 }).then((user) => {
+    console.log(user);
+    // count the users with the same age
+    return User.countDocuments({ age: 26})
+}).then(result => {
+    console.log(result)
+}).catch(e => console.log(e));
