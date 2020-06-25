d2c368c82c4db58f674049daf9b1f7fe85eede3e
Isaac Obuya <Szack4477@gmail.com>
2020-06-25 15:17:23 +0300

    refactor away user routes to separate file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // ensures that the file mongoose js runs
 require('./db/mongoose');
 // model
-const User = require('./models/user');
+const userRouter = require("./routers/user");
 const Task = require("./models/task");
 
 const app = express();
@@ -13,17 +13,10 @@ const port = process.env.PORT || 3000;
 // we need to configure express to automatically parse for us the 
 // JSON information passed in request body
 app.use(express.json()); // will automatically parse the incoming JSON data into an object that we can the save into database
-app.post('/users', async (req, res) => {
-    // grabbing incoming body data
-    const user = new User(req.body);
-    try {
-    // save the new user
-    await user.save();
-    res.status(201).send(user);
-    } catch(error) {
-        res.status(400).send(error);
-    }
-});
+
+// we have to register router with express application
+app.use(userRouter);
+
 
 // create a new post
 app.post('/tasks', async (req, res) => {
@@ -36,78 +29,7 @@ app.post('/tasks', async (req, res) => {
     }
 });
 
-// fetching multiple users
-app.get('/users', async (req, res) => {
-    try {
-    // fetch all users stored in the database
-     const users = await User.find({});
-     res.status(200).send(users);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
 
-// express provides us with route parameters
-// part of the Url that are used to capture dynamic values
-app.get('/users/:id', async (req, res) => {
-    // need to access the param provided
-    const _id = req.params.id;
-    try {
-    // fetch the a single user
-    // in mongoose, there are two methods - findOne and findById
-    const user = await User.findById(_id);
-    // remember, a mongodb query is not considered a failure if we don't find something
-     if (!user) {
-         console.log("user not found");
-         return res.status(404).send();
-     }       
-     res.send(user); // automatically returns status of 200
-    } catch(error) {
-        res.status(500).send();
-    }
-});
-
-// updating an existing user
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    // we don't want to update fields that do not exist
-    const allowedUpdates = ['name', 'email', 'password', 'age'];
-    const isValidUpdate = updates.every(update => allowedUpdates.includes(update));
-
-    if (!isValidUpdate) {
-        return res.status(400).send({error: 'Invalid updates'});
-    }
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true, // return new user as opposed to existing one that was found before
-            runValidators: true // ensure data is validated
-        });
-
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-})
-
-// deleting a user
-app.delete('/users/:id', async (req, res) => {
-
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
 
 // fetch multiple tasks
 app.get('/tasks', (req, res) => {
---
+++ src/routers/user.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const User = require('../models/user');
+
+// creating a new router
+const router = new express.Router();
+
+// router has access to the same methods we have worked with
+router.post('/users', async (req, res) => {
+    // grabbing incoming body data
+    const user = new User(req.body);
+    try {
+    // save the new user
+    await user.save();
+    res.status(201).send(user);
+    } catch(error) {
+        res.status(400).send(error);
+    }
+});
+
+// fetching multiple users
+router.get('/users', async (req, res) => {
+    try {
+    // fetch all users stored in the database
+     const users = await User.find({});
+     res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// express provides us with route parameters
+// part of the Url that are used to capture dynamic values
+router.get('/users/:id', async (req, res) => {
+    // need to access the param provided
+    const _id = req.params.id;
+    try {
+    // fetch the a single user
+    // in mongoose, there are two methods - findOne and findById
+    const user = await User.findById(_id);
+    // remember, a mongodb query is not considered a failure if we don't find something
+     if (!user) {
+         console.log("user not found");
+         return res.status(404).send();
+     }       
+     res.send(user); // automatically returns status of 200
+    } catch(error) {
+        res.status(500).send();
+    }
+});
+
+// updating an existing user
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    // we don't want to update fields that do not exist
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidUpdate = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidUpdate) {
+        return res.status(400).send({error: 'Invalid updates'});
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true, // return new user as opposed to existing one that was found before
+            runValidators: true // ensure data is validated
+        });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+// deleting a user
+router.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
