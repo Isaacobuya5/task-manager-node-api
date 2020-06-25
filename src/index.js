716c436d6facc10934ef425d7801ab6bfff81bf5
Isaac Obuya <Szack4477@gmail.com>
2020-06-25 13:31:45 +0300

    refactor get /users/:id to use async and await

--- src/index.js
+++ src/index.js
@@ -49,21 +49,22 @@ app.get('/users', async (req, res) => {
 
 // express provides us with route parameters
 // part of the Url that are used to capture dynamic values
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     // need to access the param provided
     const _id = req.params.id;
+    try {
     // fetch the a single user
     // in mongoose, there are two methods - findOne and findById
-    User.findById(_id).then((user) => {
+    const user = await User.findById(_id);
     // remember, a mongodb query is not considered a failure if we don't find something
      if (!user) {
          console.log("user not found");
          return res.status(404).send();
      }       
      res.send(user); // automatically returns status of 200
-    }).catch((error) => {
+    } catch(error) {
         res.status(500).send();
-    })
+    }
 });
 
 // fetch multiple tasks
