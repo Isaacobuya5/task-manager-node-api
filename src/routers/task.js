b1454bed81fcbecf9ea2c22c26c199d0ad566c17
Isaac Obuya <Szack4477@gmail.com>
2020-07-01 13:56:45 +0300

    pass auth middleware to GET /task:id to enable user fetch specifically task they created

--- src/routers/task.js
+++ src/routers/task.js
@@ -32,10 +32,11 @@ router.get('/tasks', (req, res) => {
 });
 
 // fetch a single task
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth,  async (req, res) => {
     const _id = req.params.id;
     try {
-        const task = await Task.findById(_id);
+        // const task = await Task.findById(_id);
+        const task = await Task.findOne({ _id, owner: req.user._id});
         if (!task) {
             return res.status(404).send();
         }
