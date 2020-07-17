90e434ede527267a440c85adafcc151984b7a2d1
Isaac Obuya <Szack4477@gmail.com>
2020-07-17 16:22:12 +0300

    send a welcome email message to a newly created user

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -1,14 +1,27 @@
 
 const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey('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')
+sgMail.setApiKey('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');
 
-const msg = {
-    to: 'szack4477@gmail.com',
-    from: 'szack4477@gmail.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'szack4477@gmail.com',
+        subject: "Thanks for creating an account",
+        text: `Welcome to the app, ${name}. Let me know how you get along!.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail
+}
+
+// const msg = {
+//     to: 'szack4477@gmail.com',
+//     from: 'szack4477@gmail.com',
+//     subject: 'Sending with SendGrid is Fun',
+//     text: 'and easy to do anywhere, even with Node.js',
+//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+//   }
   
 
-sgMail.send(msg).catch(error => console.log(error));
+// sgMail.send(msg).catch(error => console.log(error));
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+const { sendWelcomeEmail } = require('../emails/accounts');
 
 // creating a new router
 const router = new express.Router();
@@ -14,6 +15,7 @@ router.post('/users', async (req, res) => {
     try {
     // save the new user
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     // generate a new token for the user
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
