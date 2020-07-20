78f00b4c2fb1469d3e2a5a7352e66fe632eddf41
Isaac Obuya <Szack4477@gmail.com>
2020-07-20 07:38:09 +0300

    more basic tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require("../src/math");
+const { calculateTip, fahrenheitToCelcius, celciusToFahrenheit } = require("../src/math");
 
 test('should calculate total with tip', () => {
     const total = calculateTip(10, .3);
@@ -11,6 +11,16 @@ test('should calculate total with tip', () => {
 test('should calculate total with default tip', () => {
     const total = calculateTip(10);
     expect(total).toBe(12.5);
+});
+
+test("Should convert 32 F to 0 C", () => {
+    const result = fahrenheitToCelcius(32);
+    expect(result).toBe(0);
+});
+
+test("Should convert 0 C to 32 F", () => {
+    const result = celciusToFahrenheit(0);
+    expect(result).toBe(32);
 })
 
 
