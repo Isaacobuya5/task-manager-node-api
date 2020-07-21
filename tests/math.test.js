645f3fc8c9c06f93e1f774161aa36130e02c2a10
Isaac Obuya <Szack4477@gmail.com>
2020-07-21 05:20:05 +0300

    setup package.json for testing

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelcius, celciusToFahrenheit } = require("../src/math");
+const { calculateTip, fahrenheitToCelcius, celciusToFahrenheit, add } = require("../src/math");
 
 test('should calculate total with tip', () => {
     const total = calculateTip(10, .3);
@@ -22,6 +22,27 @@ test("Should convert 0 C to 32 F", () => {
     const result = celciusToFahrenheit(0);
     expect(result).toBe(32);
 })
+
+test("Async test demo", (done) => {
+    setTimeout(() => {
+        expect(1).toBe(1)
+        done()
+    },2000)
+})
+
+test("Should add two numbers", (done) => {
+    add(2,3).then((sum) => {
+        expect(sum).toBe(5);
+        done();
+    })
+});
+
+test("Should add two numbers async await", async () => {
+    const sum = await add(10, 22);
+    expect(sum).toBe(32);
+})
+
+
 
 
 
